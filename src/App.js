146e67dfba7ddd976b511bf4c513524f16032761
Alfonso <alfonso.garcia2324@gmail.com>
2019-05-20 14:18:15 -0700

    Finished creating signup and login pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import HomepageView from "./views/HomepageView";
 import LoginView from "./views/LoginView";
+import SignupView from "./views/SignupView";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
         <Navigation />
 
         <Route exact path="/" component={HomepageView} />
+        <PrivateRoute exact path="/dashboard" component={HomepageView} />
         <Route path="/login" component={LoginView} />
+        <Route path="/signup" component={SignupView} />
       </div>
     </Router>
   );
--- src/views/SignupView.js
+++ src/views/SignupView.js
@@ -12,8 +12,16 @@ class LoginView extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className="login-form">
-        <h3>Log In:</h3>
+      <form onSubmit={this.handleSubmit} className="login-form col s12">
+        <h3>Sign Up:</h3>
+        <div className="input-field col s6">
+          <input id="first-name" type="text" />
+          <label for="first-name">First Name</label>
+        </div>
+        <div className="input-field col s6">
+          <input id="last-name" type="text" />
+          <label for="last-name">Last Name</label>
+        </div>
         <div className="input-field">
           <input id="username" type="text" />
           <label for="username">Username</label>
