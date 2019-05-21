07ee748eca086c7d4bdc506e1931bedfb330a26e
Alfonso <alfonso.garcia2324@gmail.com>
2019-05-21 08:38:45 -0700

    Sign up and login function correctly as of now

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -15,10 +15,27 @@ export const SIGN_UP_FAILED = "SIGN_UP_FAILED";
 // Action Creators ---
 export const login = creds => dispatch => {
   dispatch({ type: LOGGING_IN });
-  console.log(creds);
+  return axios
+    .post("https://lambda-sleeptracker.herokuapp.com/api/login", creds)
+    .then(res => {
+      console.log(res);
+      dispatch({ type: LOGIN_SUCCESSFUL, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err.response);
+    });
 };
 
 export const signup = creds => dispatch => {
   dispatch({ type: SIGNING_UP });
-  console.log(creds);
+  return axios
+    .post("https://lambda-sleeptracker.herokuapp.com/api/register", creds)
+    .then(res => {
+      console.log(res);
+      dispatch({ type: SIGN_UP_SUCCESSFUL, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: SIGN_UP_FAILED });
+    });
 };
--- app/src/views/SignupView.js
+++ app/src/views/SignupView.js
@@ -26,7 +26,9 @@ class SignupView extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.props.signup(this.state.credentials);
+    this.props.signup(this.state.credentials).then(() => {
+      this.props.history.push("/dashboard");
+    });
   };
 
   render() {
